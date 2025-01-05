7d5571b2795a6f6ddd01b8abe50a782791cad23f
maurice filiatreault <inbox@mauricefh.com>
2025-01-05 17:52:06 -0500

    Added files and delete files

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -287,10 +287,15 @@ globalkeys = gears.table.join(
     { description = "lua execute prompt", group = "awesome" }),
 
   -- Customs
-    awful.key({ modkey, "Shift" }, "b", function() awful.spawn("brave") end,
+    awful.key({ modkey, "Shift" }, "b", function() awful.spawn("Zen Browser") end,
     { description = "open a terminal", group = "launcher" }),
     awful.key({}, "XF86AudioMicMute", function () awful.spawn("amixer set Capture toggle") end,
-    { description = "Mute microphone", group = "media" })
+    { description = "Mute microphone", group = "media" }),
+    awful.key({}, "XF86MonBrightnessDown", function () awful.spawn("brightnessctl set 5%-") end,
+    { description = "Decrease brightness by 5%", group = "media" }),
+        awful.key({}, "XF86MonBrightnessUp", function () awful.spawn("brightnessctl set +5%") end,
+    { description = "Increase brightness by 5%", group = "media" })
+
 )
 
 clientkeys = gears.table.join(
@@ -427,24 +432,11 @@ awful.rules.rules = {
   {
     rule_any = {
       instance = {
-        "DTA",     -- Firefox addon DownThemAll.
-        "copyq",   -- Includes session name in class.
-        "pinentry",
       },
       class = {
         "Arandr",
-        "Blueman-manager",
-        "Gpick",
-        "Kruler",
-        "MessageWin",    -- kalarm.
-        "Sxiv",
-        "Tor Browser",   -- Needs a fixed window size to avoid fingerprinting by screen size.
-        "Wpa_gui",
-        "veromix",
-        "xtightvncviewer" },
-
-      -- Note that the name property shown in xprop might be set slightly after creation of the client
-      -- and the name shown there might not match defined rules here.
+        "Blueman-manager"
+        },
       name = {
         "Event Tester",   -- xev.
       },
