9e89368f15c19a2f3fd40bbaff6137f45c442987
Maurice Filiatreault-Hebert <inbox@mauricefh.com>
2025-10-07 18:06:27 -0600

    update

--- .local/bin/clean.sh
+++ .local/bin/clean.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Clean package manager cache
 sudo pacman -Scc
-
 sudo pacman -Qtdq | sudo pacman -Rns -
+
+# Clean command and clipboard
+echo "" > ~/.bash_history
+clipcatctl clear
+trash-empty
